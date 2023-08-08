8c9c21d761cedd3109e310dfef1485853b201541
GUIRA LOUCKMANE <guiralouckmane508@gmail.com>
2023-08-08 16:26:45 +0000

    TP19_GUIRA_LOUCKMANE

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const trois=document.querySelector(".trois")
 const cou=document.querySelector(".cou")
 const btn2=document.querySelector(".btn2")
 const btn3=document.querySelector(".btn3")
-const go1=document.querySelector("go1")
-
+const go1=document.querySelector(".go1")
+const go2=document.querySelector(".go2")
+const go3=document.querySelector(".go3")
+const look=document.querySelector(".look")
+const lock=document.querySelector(".lock")
 
 love.addEventListener("click",() => {
     header.style.display="none"
@@ -30,18 +33,50 @@ btn2.addEventListener("click",() =>{
     trois.style.display="none"
 }
                       )
-btn3.addEventListener("click",() =>{
-    deux.style.display="none"
-    header.style.display="none"
-    trois.style.display="block"
-}
+// btn3.addEventListener("click",() =>{
+    // header.style.display="block"
+    // deux.style.display="block"
+    // trois.style.display="block"
+// }
                       
-                      )
+                    //   )
 
-btn.addEventListener("click",() =>{
-    go1.style.display="block"
+btn3.addEventListener("click",() =>{
+    deux.style.display="none";
+    header.style.display="none";
+    trois.style.display="block";
+    }
+)
+                      go2.addEventListener("click",() =>{
     
-}
-                      )
+                        deux.style.display="block"
+                        header.style.display="none"
+                        trois.style.display="none"
+                    }
+                                          )
+                                          
+                                          go3.addEventListener("click",() =>{
+                                            deux.style.display="none";
+                                            header.style.display="none";
+                                            trois.style.display="block";
+                                            }
+                                        )                   
 
-
+                                        love.addEventListener("click",() =>{
+                                            deux.style.display="block";
+                                            header.style.display="none";
+                                            trois.style.display="none";
+                                            }
+                                        )                   
+                                        look.addEventListener("click",() =>{
+                                            deux.style.display="block";
+                                            header.style.display="none";
+                                            trois.style.display="none";
+                                            }
+                                        )
+                                        Lock.addEventListener("click",() =>{
+                                            deux.style.display="block";
+                                            header.style.display="none";
+                                            trois.style.display="none";
+                                            }
+                                        )                                   
